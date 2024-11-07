32f3910b6c2fc9a3e4978c540d47a74176a20368
Sojol Rana <48207446+sojolrana@users.noreply.github.com>
2024-11-07 16:00:03 +0600

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h2>About Me</h2>
+      <p>I am Sojol Rana, a passionate programmer with skills in various technologies including React, Dart, and JavaScript.</p>
+    </div>
+  );
+}
+
+export default About;
